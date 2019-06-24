d009b46433f42bfdcc5fc20cc449b12c79005778
Okan Bulut <okanbulut84@gmail.com>
2019-06-24 15:29:32 +0300

    Add files via upload

---
+++ packages_to_install.R
@@ -0,0 +1,7 @@
+
+# Packages to be used
+packages <- c("data.table", "tidyverse","e1071", "caret", "mlr",
+              "modelr", "randomForest", "rpart", "rpart.plot", 
+              "GGally", "ggExtra")
+
+install.packages(packages)
